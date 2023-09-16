f9344e0b678500184c2af23b470390b367838f52
Marshall Bowers <elliott.codes@gmail.com>
2023-09-15 22:05:37 -0400

    Add basic `manifest`

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use async_trait::async_trait;
+
+pub trait Manifest {
+    fn manifest() -> Self;
+}
+
+#[async_trait]
+pub trait Persist: Manifest {
+    async fn persist() -> Self;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[derive(Debug, PartialEq, Eq)]
+    struct Movie {
+        pub title: String,
+        pub year: u32,
+    }
+
+    impl Manifest for Movie {
+        fn manifest() -> Self {
+            Self {
+                title: "Inception".into(),
+                year: 2010,
+            }
+        }
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn manifest_works() {
+        let movie = Movie::manifest();
+
+        assert_eq!(
+            movie,
+            Movie {
+                title: "Inception".into(),
+                year: 2010
+            }
+        )
     }
 }
