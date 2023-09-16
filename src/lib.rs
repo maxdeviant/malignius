384d470dcc547fb5697f64c39a3ef465222ea95b
Marshall Bowers <elliott.codes@gmail.com>
2023-09-15 22:21:30 -0400

    Add support for passing `overrides` to `manifest`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use async_trait::async_trait;
 
 pub trait Manifest {
-    fn manifest() -> Self;
+    type Overrides;
+
+    fn manifest(overrides: Self::Overrides) -> Self;
 }
 
 #[async_trait]
@@ -11,31 +13,52 @@ pub trait Persist: Manifest {
 
 #[cfg(test)]
 mod tests {
+    use derive_builder::Builder;
+
     use super::*;
 
-    #[derive(Debug, PartialEq, Eq)]
+    #[derive(Debug, Builder, PartialEq, Eq)]
     struct Movie {
         pub title: String,
         pub year: u32,
     }
 
     impl Manifest for Movie {
-        fn manifest() -> Self {
+        type Overrides = MovieBuilder;
+
+        fn manifest(builder: Self::Overrides) -> Self {
             Self {
-                title: "Inception".into(),
-                year: 2010,
+                title: builder.title.unwrap_or("Inception".into()),
+                year: builder.year.unwrap_or(2010),
             }
         }
     }
 
     #[test]
     fn manifest_works() {
-        let movie = Movie::manifest();
+        let movie = Movie::manifest(MovieBuilder::default());
 
         assert_eq!(
             movie,
             Movie {
                 title: "Inception".into(),
+                year: 2010
+            }
+        )
+    }
+
+    #[test]
+    fn manifest_works_with_overrides() {
+        let movie = Movie::manifest({
+            let mut movie = MovieBuilder::default();
+            movie.title("The Social Network".into());
+            movie
+        });
+
+        assert_eq!(
+            movie,
+            Movie {
+                title: "The Social Network".into(),
                 year: 2010
             }
         )
