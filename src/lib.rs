711e5cd7c677c688987f3453c758e51435776141
Marshall Bowers <elliott.codes@gmail.com>
2023-09-15 22:44:01 -0400

    Implement `persist`

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,17 @@ pub trait Manifest {
 }
 
 #[async_trait]
-pub trait Persist: Manifest {
-    async fn persist() -> Self;
+pub trait Persist: Manifest + Sized {
+    type Conn;
+    type Err;
+
+    async fn persist(conn: &mut Self::Conn) -> Result<Self, Self::Err>;
 }
 
 #[cfg(test)]
 mod tests {
     use derive_builder::Builder;
+    use rusqlite::{params, Connection};
 
     use super::*;
 
@@ -31,6 +35,25 @@ mod tests {
                 title: builder.title.unwrap_or("Inception".into()),
                 year: builder.year.unwrap_or(2010),
             }
+        }
+    }
+
+    #[async_trait]
+    impl Persist for Movie {
+        type Conn = Connection;
+        type Err = rusqlite::Error;
+
+        async fn persist(conn: &mut Self::Conn) -> Result<Self, Self::Err> {
+            let movie = Movie::manifest(MovieBuilder::default());
+
+            conn.execute(
+                "
+                    insert into movie (title, year) values ($1, $2)
+                ",
+                params![movie.title, movie.year],
+            )?;
+
+            Ok(movie)
         }
     }
 
@@ -62,5 +85,48 @@ mod tests {
                 year: 2010
             }
         )
+    }
+
+    #[tokio::test]
+    async fn persist_works() -> Result<(), Box<dyn std::error::Error>> {
+        let mut conn = Connection::open(":memory:")?;
+
+        conn.execute(
+            r#"
+                create table if not exists movie (
+                    id integer primary key,
+                    title text not null unique,
+                    year integer not null
+                );
+            "#,
+            (),
+        )?;
+
+        let movie = Movie::persist(&mut conn).await?;
+
+        assert_eq!(
+            movie,
+            Movie {
+                title: "Inception".into(),
+                year: 2010
+            }
+        );
+
+        let persisted_movie = conn.query_row(
+            "
+                select title, year from movie where title = $1
+            ",
+            [movie.title.clone()],
+            |row| {
+                Ok(Movie {
+                    title: row.get(0).unwrap(),
+                    year: row.get(1).unwrap(),
+                })
+            },
+        )?;
+
+        assert_eq!(movie, persisted_movie);
+
+        Ok(())
     }
 }
